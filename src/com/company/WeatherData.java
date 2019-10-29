0ba3493e30592e542f2b7a43f50fbd2dbdc5f88b
Ozan Yıldız <developer@yildizozan.com>
2019-10-29 21:07:33 +0300

    Initial commit

---
+++ src/com/company/WeatherData.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class WeatherData implements Subject {
+	private ArrayList observers;
+	private float temperature;
+	private float humidity;
+	private float pressure;
+
+	public WeatherData() {
+		observers = new ArrayList();
+	}
+
+	@Override
+	public void registerObserver(Observer o) {
+		observers.add(o);
+	}
+
+	@Override
+	public void removeObserver(Observer o) {
+		int i = observers.indexOf(o);
+		if (0 <= i) {
+			observers.remove(i);
+		}
+	}
+
+	@Override
+	public void notifyObserver() {
+		for (int i = 0; i < observers.size(); i++) {
+			Observer observer = (Observer) observers.get(i);
+			observer.update(this.temperature, this.humidity, this.pressure);
+		}
+	}
+
+	public void measurementsChanged() {
+		notifyObserver();
+	}
+
+	public void setMeasurements(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.pressure = pressure;
+		measurementsChanged();
+	}
+
+
+}
